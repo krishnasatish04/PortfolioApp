f82ab9905245e3db72e9416a20f930034cdd9fed
krishnasatish04 <krishnasatish04@gmail.com>
2023-06-16 14:14:16 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Portfolio App...</h1>
+    </div>
+  );
+}
+
+export default App;
